53698b107317af33a2562037d69b01f6f057c078
jayak <kumarijaya127@gmail.com>
2024-11-18 17:12:19 +0530

    cors updated

--- liveWeather-App-backend/server.js
+++ liveWeather-App-backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // app.use(cors());
 app.use(
   cors({
-    origin: "https://weather-live-app-live.netlify.app/",
+    origin: process.env.FRONTEND_URL,
   })
 );
 
