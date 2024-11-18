493c36f5974f1c01844d4de26e6b3b83bbf22bad
jayak <kumarijaya127@gmail.com>
2024-11-18 16:55:46 +0530

    Updated CORS configuration

--- liveWeather-App-backend/server.js
+++ liveWeather-App-backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // app.use(cors());
 app.use(
   cors({
-    origin: "https://your-frontend-url.netlify.app",
+    origin: "https://weather-live-app-live.netlify.app/",
   })
 );
 
